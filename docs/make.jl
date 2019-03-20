474178821da8e4af7c05f20480af6ee15acbb622
Pierre Navaro <pnavaro@users.noreply.github.com>
2019-03-20 19:19:38 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,7 @@ makedocs(
     modules = [GEMPIC]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/juliavlasov/GEMPIC.jl.git",
+ )
