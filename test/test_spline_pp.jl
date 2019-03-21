e172fd99b3c2fd7952e88605c2c195eca91c874a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-03-21 14:04:10 +0100

    Update test_spline_pp.jl

--- test/test_spline_pp.jl
+++ test/test_spline_pp.jl
@@ -1,15 +1,11 @@
 @testset " spline pp 1d " begin
 
-import GEMPIC: SplinePP
+import GEMPIC: SplinePP, b_to_pp, uniform_bsplines_eval_basis, horner_1d
 using Random
 
 ncells = 8
 degree = 3
   
-b_coeffs  = zeros(Float64, ncells)
-pp_coeffs = zeros(Float64,(degree+1,ncells))
-val       = zeros(Float64, degree+1)
-
 domain    = [0., 2π]
 delta_x   = (domain[2] - domain[1]) / ncells
  
@@ -18,12 +14,12 @@ b_coeffs  = rand(Float64,ncells)
  
 spline_pp = SplinePP( degree, ncells)
 
-b_to_pp(spline_pp, n_cells, b_coeffs, pp_coeffs)
+pp_coeffs = b_to_pp(spline_pp, ncells, b_coeffs)
 
 xp = rand() * (domain[2]-domain[1])
 
 xi    = (xp - domain[1])/delta_x
-index = floor(xi)+1
+index = floor(Int64,xi)+1
 xi    = xi - (index-1)
 res   = horner_1d(degree, pp_coeffs, xi, index)
     
@@ -34,15 +30,16 @@ val = uniform_bsplines_eval_basis(degree, xi)
 res2 = 0.0
 
 for i = 1:degree+1 
-   index1d = modulo(index+i-2, n_cells)+1
-   res2 = res2 + b_coeffs(index1d) * val(i)
+   index1d = (index+i-2) % ncells + 1
+   res2    = res2 + b_coeffs[index1d] * val[i]
 end 
 
 @test abs(res-res2) < 1e-15
 
 @test spline_pp.degree - degree < 1e-15
 
-res = horner_1d(degree, pp_coeffs, rand(), 1)
+index = 1
+res = horner_1d(degree, pp_coeffs, rand(), index)
 
 res2 = 0.
 for i=1:degree+1
