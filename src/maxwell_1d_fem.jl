048a459b9b2a884078c570b471bea2a0a2b6715b
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-07-11 17:13:10 +0200

    Update maxwell_1d_fem.jl

--- src/maxwell_1d_fem.jl
+++ src/maxwell_1d_fem.jl
@@ -310,6 +310,8 @@ end
 export l2projection!
 
 """
+    l2projection!(coefs_dofs, maxwell, func, degree)
+
 Compute the L2 projection of a given function f on periodic splines 
 of given degree
 """
