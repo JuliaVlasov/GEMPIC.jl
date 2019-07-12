74f6b78829874d3b4bca3243e7e66f9f8cbd36e4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-07-12 16:16:00 +0200

    Update distributions.jl

--- notebooks/distributions.jl
+++ notebooks/distributions.jl
@@ -7,18 +7,35 @@
 #       extension: .jl
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.1
+#       jupytext_version: 1.1.7
 #   kernelspec:
 #     display_name: Julia 1.1.0
 #     language: julia
 #     name: julia-1.1
 # ---
 
+# +
+using Plots
+
+nx, nv = 64, 128
+xmin, xmax = 0, 2π/kx
+vmin, vmax = -6, 6
+xg = range(xmin, stop=xmax, length=nx+1)[1:end-1] |> collect
+vg = range(vmin, stop=vmax, length=nx+1)[1:end-1] |> collect;
+
+# +
+kx, ϵ = 0.5, 0.1
+
+fxv(x, v) = ( 1 + ϵ * cos(kx * x )) / sqrt(2π) * exp(- (v^2)/ 2)
+
+surface(xg, vg, fxv)
+# -
+
 using Distributions
 using Random
 
 # $$
-#  f(x,v) = \frac{1}{\sqrt{2π}}( 1 + ϵ \cos ( k_x x)  e^{-v^2/2} 
+#  f(x,v) = \frac{1}{\sqrt{2π}} \big( 1 + ϵ \cos ( k_x x) \big) e^{-v^2/2} 
 # $$
 
 ?Cosine
@@ -30,22 +47,10 @@ using Random
 
 ?Normal
 
-kₓ, ϵ = 0.5, 0.1
-
-using Plots
-
-nx, nv = 64, 128
-xmin, xmax = 0, 2π
-vmin, vmax = -6, 6
-xg = range( 0, stop=2, length=nx+1)[1:end-1] |> collect
-vg = range(-6, stop=6, length=nx+1)[1:end-1] |> collect;
-
 fx = Cosine(1,1)
 fv = Normal(0,1)
 
 plot(vg, pdf.(fv, vg))
-
-rand(fx)
 
 x = rand!(fx, zeros(10000))
 v = rand!(fv, zeros(10000));
@@ -53,6 +58,4 @@ v = rand!(fv, zeros(10000));
 Plots.histogram(x, normalize=true, bins = 100, fill=:slategray, lab = "draws")
 plot!(xg, pdf.(fx, xg))
 
-fxv(x, v) = ( 1 + ϵ * cos(kₓ * x )) / sqrt(2π) * exp(- (v^2)/ 2)
 
-surface(xg, vg, fxv)
