b3d2d987a1b6c2769bdaf62d2ffea5763b04c176
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-07-12 16:16:13 +0200

    Update quietstart.jl

--- notebooks/quietstart.jl
+++ notebooks/quietstart.jl
@@ -7,7 +7,7 @@
 #       extension: .jl
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.1
+#       jupytext_version: 1.1.7
 #   kernelspec:
 #     display_name: Julia 1.1.0
 #     language: julia
