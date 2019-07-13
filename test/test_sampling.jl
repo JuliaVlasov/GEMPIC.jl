c3c081c3ebb24824db845bb4b4f249a61fe6910d
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-07-13 12:03:24 +0200

    Update test_sampling.jl

--- test/test_sampling.jl
+++ test/test_sampling.jl
@@ -106,8 +106,7 @@ for j=1:2
                      + (df2.params.delta[2] 
                      -  df2.params.delta[1]) * df2.params.v_mean[j][2])^2
     for k=1:2
-        sigma_ref[j+1] += df2.params.delta[k] 
-                        * (df2.params.v_thermal[j][k]^2 + df2.params.v_mean[j][k]^2)
+        sigma_ref[j+1] += df2.params.delta[k] * (df2.params.v_thermal[j][k]^2 + df2.params.v_mean[j][k]^2)
     end
 end
   
