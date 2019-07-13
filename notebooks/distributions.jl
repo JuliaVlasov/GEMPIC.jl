47a7c4498a59a6c75fbaeb554ec1f747081af2d6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-07-13 17:57:53 +0200

    Update distributions.jl

--- notebooks/distributions.jl
+++ notebooks/distributions.jl
@@ -3,6 +3,7 @@
 # jupyter:
 #   jupytext:
 #     comment_magics: false
+#     formats: ipynb,jl:light
 #     text_representation:
 #       extension: .jl
 #       format_name: light
@@ -14,30 +15,30 @@
 #     name: julia-1.1
 # ---
 
+include("../src/distributions.jl")
 
-
-
+?SumCosGaussian
 
 # +
-using Plots, GEMPIC
+using Plots
 
-kx, ϵ = 0.5, 0.1
+k, α = 0.5, 0.1
 nx, nv = 64, 128
-xmin, xmax = 0, 2π/kx
+xmin, xmax = 0, 2π/k
 vmin, vmax = -6, 6
 xg = range(xmin, stop=xmax, length=nx+1)[1:end-1] |> collect
 vg = range(vmin, stop=vmax, length=nv+1)[1:end-1] |> collect;
 
 # -
 
-df_1d1v = SumCosGaussian{1,1}( 1, 1, [[kx]] , [ϵ] , [[1.0]] , [[0.0]], [1.0])
+df_1d1v = SumCosGaussian{1,1}( [[k]] , [α] , [[1.0]] , [[0.0]], [1.0])
 f = zeros(Float64, (nx,nv))
 for  j in eachindex(vg), i in eachindex(xg)
     f[i,j] = df_1d1v( xg[i], vg[j])
 end
 surface(xg, vg, f')
 
-two_gaussians = SumCosGaussian{1,1}( 1, 2, [[kx]] , [0.0] , [[0.2],[0.2]] , [[-1.0],[1.0]], [0.5, 0.5])
+two_gaussians = SumCosGaussian{1,1}( [[k]] , [0.0] , [[0.2],[0.2]] , [[-1.0],[1.0]], [0.5, 0.5])
 f = zeros(Float64, (nx,nv))
 for  j in eachindex(vg), i in eachindex(xg)
     f[i,j] = two_gaussians( xg[i], vg[j])
@@ -45,24 +46,15 @@ end
 surface(xg, vg, f')
 
 # +
-
-
-fxv(x, v) = ( 1 + ϵ * cos(kx * x )) / sqrt(2π) * exp(- (v^2)/ 2)
+fxv(x, v) = ( 1 + α * cos(k * x )) / sqrt(2π) * exp(- (v^2)/ 2)
 
 surface(xg, vg, fxv)
 # -
 
-?SumCosGaussian
+?CosSumGaussian
 
-v_thermal = hcat([1.0])
-v_mean    = hcat([0.0])
-δ = 1.0
-df = CosSumOneGaussian( (1,1), 1, 1, hcat([kx]), [0.1], v_thermal, v_mean, δ )
+df = CosSumGaussian{1,1}( [[k]], [0.1], [[1.0]], [[0.0]], [1.0] )
 
 plot( xg, [eval_x_density(df, x) for x in xg])
 
 plot( vg, [eval_v_density(df, v) for v in vg])
-
-surface( xg, vg, df(xg, vg))
-
-
