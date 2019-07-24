425d96e9e15b3dde167b7a76d4ee15ed26605390
Pierre Navaro <pnavaro@users.noreply.github.com>
2019-07-24 11:04:01 -0400

    Update maxwell_1d_fem.jl

--- src/maxwell_1d_fem.jl
+++ src/maxwell_1d_fem.jl
@@ -15,7 +15,7 @@ export Maxwell1DFEM
 - `delta_x`              : cell size
 - `n_dofs`               : number of cells (and grid points)
 - `s_deg_0`              : spline degree 0-forms
-- `s_deg_1              : spline degree 1-forms
+- `s_deg_1`              : spline degree 1-forms
 - `mass_0`               : coefficients of 0-form mass matrix
 - `mass_1`               : coefficients of 1-form mass matrix
 - `eig_mass0`            : eigenvalues of circulant 0-form mass matrix
