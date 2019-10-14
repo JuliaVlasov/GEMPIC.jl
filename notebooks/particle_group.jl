0bc6e4801b4ea9ebc4b051f30107ba84ec1e68d1
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-14 17:06:02 +0200

    Update particle_group.jl

--- notebooks/particle_group.jl
+++ notebooks/particle_group.jl
@@ -15,6 +15,8 @@
 #     name: julia-1.2
 # ---
 
+using Test, Plots
+
 include("../src/mesh.jl")
 include("../src/low_level_bsplines.jl")
 include("../src/splinepp.jl")
@@ -29,13 +31,10 @@ include("../src/hamiltonian_splitting_boris.jl")
 include("../src/diagnostics.jl")
 
 # +
-using Test
-
-
 function test_sampling( sampling_type :: Symbol, 
                         symmetric     :: Bool, 
                         pg            :: ParticleGroup{D,V}, 
-                        df            :: CosSumOneGaussian ) where {D, V}
+                        df            :: AbstractCosGaussian ) where {D, V}
 
    mean  = zeros(3)
    sigma = zeros(3)
@@ -43,9 +42,9 @@ function test_sampling( sampling_type :: Symbol,
 
    n_particles = pg.n_particles
    
-   sampling = ParticleSampler( sampling_type, symmetric, (D, V), n_particles )
+   sampling = ParticleSampler{D,V}( sampling_type, symmetric, n_particles )
 
-   sample( sampling, pg, df, mesh )
+   sample!( pg, sampling, df, mesh )
    
    for i_part = 1:n_particles
        xi = get_x(pg, i_part)
@@ -68,10 +67,10 @@ function test_sampling( sampling_type :: Symbol,
    sigma = sigma/(n_particles-1)
    
    mean, sigma
-   
-end
 
-# +
+end
+# -
+
 n_particles = 100000
 xmin        = 1.0 :: Float64
 xmax        = 4π + 1.0
@@ -82,20 +81,16 @@ mesh = Mesh( xmax, xmin, nx)
 
 pg = ParticleGroup{1,2}(n_particles, 1.0, 1.0, 1)
 
-params = ( dims        = (1,2),
-           n_cos       = 1,
-           n_gaussians = 1,
-           kx          = hcat([0.5]),
-           alpha       = [0.01],
-           v_thermal   = hcat([0.1, 2.0]),
-           v_mean      = hcat([0.0, 0.0]),
-           δ           = 0.0
+params = ( k = [[0.5]],
+           α = [0.01],
+           σ = [[0.1, 2.0]],
+           μ = [[0.0, 0.0]]
 )
 
 df1 = CosSumGaussian{1,2}(params...)
 
 mean_ref  = [Lx*0.5+xmin, 0.0, 0.0]
-sigma_ref = [Lx^2/12.0,   df1.v_thermal[1,1]^2, df1.v_thermal[2,1]^2 ]
+sigma_ref = [Lx^2/12.0,   params.σ[1][1]^2, params.σ[1][2]^2 ]
 
 @info "Sobol non symmetric"
 mean, sigma = test_sampling( :sobol, false, pg, df1 )
@@ -120,37 +115,42 @@ mean, sigma = test_sampling( :random, true,  pg, df1)
 @show abs.(mean  .- mean_ref)
 @show abs.(sigma .- sigma_ref)
 @test maximum(abs.(mean .- mean_ref)) ≈ 0.0 atol = 1e-12
-   
+
+xp = vcat([get_x(pg, i) for i in 1:n_particles]...)
+wp = vcat([get_weights(pg, i) for i in 1:n_particles]...);
+
+histogram(xp, weights=wp, normalize=true, bins=100)
+
+
+
+
+
 # Expected mean:
 # 2π+1, 0, 0
 # Expected variance:
 # 16/12 π^2 , 0.01, 4
 
-# +
 params = (
-    dims        = (1,2),
-    n_cos       = 1,
-    n_gaussians = 2,
-    kx          = hcat([0.5]),
-    alpha       = [0.01],
-    v_thermal   = hcat([0.1, 2.0], [2.0, 2.0]),
-    v_mean      = hcat([0.0, 0.0], [1.0, 1.0]),
-    δ           = 0.7
+    k = [[0.5]],
+    α = [0.01],
+    σ = [[0.1, 2.0], [2.0, 2.0]],
+    μ = [[0.0, 0.0], [1.0, 1.0]],
+    δ = [0.7, 0.3]
 )
 
-df2 = CosSumGaussian(params...)
+df2 = CosSumGaussian{1,2}(params...)
 
 mean_ref = [Lx*0.5+xmin, 0.3, 0.3]
 sigma_ref[1] = Lx^2/12.0
 for j=1:2
-    sigma_ref[j+1] = - (df2.delta[1] * df2.v_mean[j,1]
-                      +(df2.delta[2] - df2.delta[1])*df2.v_mean[j,2])^2
+    sigma_ref[j+1] = - (df2.params.δ[1] * df2.params.μ[j][1]
+                     + (df2.params.δ[2] 
+                     -  df2.params.δ[1]) * df2.params.μ[j][2])^2
     for k=1:2
-        sigma_ref[j+1] += df2.delta[k] * (df2.v_thermal[j,k]^2
-                                       +  df2.v_mean[j,k]^2)
+        sigma_ref[j+1] += df2.params.δ[k] * (df2.params.σ[j][k]^2 + df2.params.μ[j][k]^2)
     end
 end
-  
+
 @info "Sobol non symmetric"
 mean, sigma =  test_sampling( :sobol, false, pg, df2) 
 @show abs.(mean  .- mean_ref)
@@ -163,6 +163,5 @@ mean, sigma =  test_sampling( :sobol, true,  pg, df2)
 @show abs.(mean  .- mean_ref)
 @show abs.(sigma .- sigma_ref)
 @test maximum(abs.(mean .- mean_ref)) ≈ 0.0 atol = 1e2/sqrt(n_particles)
-# -
 
 
