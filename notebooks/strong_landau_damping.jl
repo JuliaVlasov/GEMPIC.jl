a92f248ccb28a645eb5e16ff09e9a2c38b9bb676
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-10 12:47:14 +0200

    Update strong_landau_damping.jl

--- notebooks/strong_landau_damping.jl
+++ notebooks/strong_landau_damping.jl
@@ -6,30 +6,17 @@
 #     text_representation:
 #       extension: .jl
 #       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
 #   kernelspec:
-#     display_name: Julia 1.2.0
+#     display_name: Julia 1.4.0
 #     language: julia
-#     name: julia-1.2
+#     name: julia-1.4
 # ---
 
-# +
 using ProgressMeter, Plots
-
-include("../src/mesh.jl")
-include("../src/distributions.jl")
-include("../src/low_level_bsplines.jl")
-include("../src/splinepp.jl")
-include("../src/maxwell_1d_fem.jl")
-include("../src/particle_group.jl")
-include("../src/particle_mesh_coupling.jl")
-include("../src/hamiltonian_splitting.jl")
-include("../src/hamiltonian_splitting_boris.jl")
-include("../src/particle_sampling.jl")
-include("../src/diagnostics.jl")
-
-# -
+using Revise
+using GEMPIC
 
 # # Strong Landau Damping
 #
@@ -54,7 +41,6 @@ mesh = Mesh( xmin, xmax, nx)
 spline_degree = 3
 
 df = CosSumGaussian{1,2}([[kx]], [α], [[σ,σ]], [[μ,μ]] )
-
 
 mass, charge = 1.0, 1.0
 particle_group = ParticleGroup{1,2}( n_particles, mass, charge, 1)   
@@ -81,6 +67,7 @@ plot!(p[2,1], v-> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
 histogram!(p[3,1], vp[:,2], weights=wp, normalize=true, bins = 100, lab = "")
 plot!(p[3,1], v-> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
 
+
 kernel_smoother1 = ParticleMeshCoupling( domain, [nx], n_particles, spline_degree-1, :galerkin)    
 kernel_smoother0 = ParticleMeshCoupling( domain, [nx], n_particles, spline_degree, :galerkin)
 rho = zeros(Float64, nx)
@@ -92,57 +79,107 @@ xg = LinRange(xmin, xmax, nx)
 sval = eval_uniform_periodic_spline_curve(spline_degree-1, rho)
 plot( xg, sval )
 
+import Pkg; Pkg.add("BenchmarkTools")
+
+using BenchmarkTools
+
+# +
+import GEMPIC.solve_poisson!
+using Base.Threads: @spawn, @sync, nthreads
+
+
+#    @sync for x_index_chunk in Iterators.partition(eachindex(x), lx ÷ 2nthreads()) 
+#        
+
+function solve_poisson!( efield_dofs       :: Vector{Float64},
+                         p    :: ParticleGroup, 
+                         kernel_smoother_0 :: ParticleMeshCoupling, 
+                         maxwell_solver    :: Maxwell1DFEM, 
+                         rho               :: Vector{Float64}) 
+
+    
+    fill!(rho, 0.0)
+
+    @sync for i_part_chunk = Iterators.partition(1:p.n_particles, nthreads())
+        @spawn begin
+            for i_part in i_part_chunk
+               xi :: Float64 = p.particle_array[1,i_part]
+               wi :: Float64 = p.charge * p.particle_array[4, i_part] * p.common_weight
+               GEMPIC.add_charge!(rho, kernel_smoother_0, xi, wi)
+            end
+        end
+    end
+
+    compute_e_from_rho!( efield_dofs, maxwell_solver, rho )
+
+end
+
+# -
+
 efield_poisson = zeros(Float64, nx)
 # Init!ialize the field solver
 maxwell_solver = Maxwell1DFEM(domain, nx, spline_degree)
 # efield by Poisson
+@show nthreads()
+@btime solve_poisson!( efield_poisson, particle_group, kernel_smoother0, maxwell_solver, rho )
+
 solve_poisson!( efield_poisson, particle_group, kernel_smoother0, maxwell_solver, rho )
 sval = eval_uniform_periodic_spline_curve(spline_degree-1, efield_poisson)
 plot( xg, sval )       
 
-# +
-# # +
 # Initialize the arrays for the spline coefficients of the fields
-efield_dofs = [efield_poisson, zeros(Float64, nx)]
-bfield_dofs = zeros(Float64, nx)
+
+# +
+function run( steps)
+
+    efield_poisson = zeros(Float64, nx)
+    # Init!ialize the field solver
+    maxwell_solver = Maxwell1DFEM(domain, nx, spline_degree)
+    # efield by Poisson
+    solve_poisson!( efield_poisson, particle_group, kernel_smoother0, maxwell_solver, rho )
     
-propagator = HamiltonianSplitting( maxwell_solver,
-                                   kernel_smoother0, 
-                                   kernel_smoother1, 
-                                   particle_group,
-                                   efield_dofs,
-                                   bfield_dofs,
-                                   domain);
-
-#propagator = HamiltonianSplittingBoris( maxwell_solver,
-#         kernel_smoother0, kernel_smoother1, particle_group,
-#         efield_dofs, bfield_dofs, domain)
-
-efield_dofs_n = propagator.e_dofs
-
-thdiag = TimeHistoryDiagnostics( particle_group, maxwell_solver, 
-                        kernel_smoother0, kernel_smoother1 );
-
-steps, Δt = 1000, 0.05
-
-@showprogress 1 for j = 1:steps # loop over time
-
-    # Strang splitting
-    strang_splitting!(propagator, Δt, 1)
-
-    # Diagnostics
-    solve_poisson!( efield_poisson, particle_group, 
-                    kernel_smoother0, maxwell_solver, rho)
+    efield_dofs = [efield_poisson, zeros(Float64, nx)]
+    bfield_dofs = zeros(Float64, nx)
+        
+    propagator = HamiltonianSplitting( maxwell_solver,
+                                       kernel_smoother0, 
+                                       kernel_smoother1, 
+                                       particle_group,
+                                       efield_dofs,
+                                       bfield_dofs,
+                                       domain)
     
-    write_step!(thdiag, j * Δt, spline_degree, 
-                    efield_dofs,  bfield_dofs,
-                    efield_dofs_n, efield_poisson)
-
+    efield_dofs_n = propagator.e_dofs
+    
+    thdiag = TimeHistoryDiagnostics( particle_group, maxwell_solver, 
+                            kernel_smoother0, kernel_smoother1 )
+    
+    Δt = 0.05
+    
+    @showprogress 1 for j = 1:steps # loop over time
+    
+        # Strang splitting
+        strang_splitting!(propagator, Δt, 1)
+    
+        # Diagnostics
+        solve_poisson!( efield_poisson, particle_group, 
+                        kernel_smoother0, maxwell_solver, rho)
+        
+        write_step!(thdiag, j * Δt, spline_degree, 
+                        efield_dofs,  bfield_dofs,
+                        efield_dofs_n, efield_poisson)
+    
+    end
+    
+    return thdiag.data
+    
 end
+
+# +
+
+@time results = run(100)
+
+plot(results[!,:Time], log.(results[!,:PotentialEnergyE1]))
 # -
-
-plot(thdiag.data[!,:Time], log.(thdiag.data[!,:PotentialEnergyE1]))
-
-
 
 
