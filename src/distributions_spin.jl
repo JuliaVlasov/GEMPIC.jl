e87ec8ba1bf91153de57715bee2c86a984ca95a7
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-04-12 11:32:31 +0200

    Update distributions_spin.jl

--- src/distributions_spin.jl
+++ src/distributions_spin.jl
@@ -1,57 +1,6 @@
 export SpinCosSumGaussian
 
 """
-    SpinCosGaussianParams( dims, k, α, σ, μ, δ ) 
-
-Parameters of a distribution with is a product of a Cosine 
-distribution along x and a Normal distribution along v.
-
-- `n_gaussians` : Number of Gaussians
-- `n_cos`       : Number of cosines
-- `normal`      : Normalization constant of each Gaussian
-"""
-struct SpinCosGaussianParams
-
-    dims        :: Tuple{Int64, Int64, Int64}
-    n_cos       :: Int64
-    n_gaussians :: Int64
-    k           :: Array{Vector{Float64}, 1}
-    α           :: Vector{Float64}
-    σ           :: Array{Vector{Float64}, 1}
-    μ           :: Array{Vector{Float64}, 1}
-    normal      :: Vector{Float64}
-    δ           :: Vector{Float64}
-
-    function SpinCosGaussianParams( dims :: Tuple{Int64, Int64, Int64},
-                                k    :: Array{Vector{Float64}, 1}, 
-                                α    :: Vector{Float64}, 
-                                σ    :: Array{Vector{Float64}, 1}, 
-                                μ    :: Array{Vector{Float64}, 1},
-                                δ    :: Vector{Float64} = [1.0]) 
-
-        n_cos = length(k) 
-        @assert n_cos == length(α)
-        for i in 1:n_cos
-            @assert length(k[i]) == dims[1]
-        end
-        n_gaussians = length(σ)
-        @assert n_gaussians == length(μ)
-        @assert all( σ .!= 0.0)
-        normal = zeros(n_gaussians)
-        for j in 1:n_gaussians
-            @assert length(σ[j]) == dims[2]
-            normal[j] = 1.0/((2π)^(0.5*dims[2])*prod(σ[j]))
-        end
-        @assert sum(δ) == 1.0
-
-        new( dims, n_cos, n_gaussians, k, α, σ, μ, normal, δ )
-    end
-
-end
-
-export SpinCosSumGaussian
-
-"""
     SpinCosSumGaussian{D,V}( n_cos, n_gaussians, k, α, σ, μ, δ )
 
 Data type for parameters of initial distribution
@@ -86,19 +35,19 @@ df = CosSumGaussian{1,1,3}([[k]],[α], [[σ₁,σ₂]], [[μ₁,μ₂]])
 ```
 
 """
-struct SpinCosSumGaussian{D, V, S} <: AbstractCosGaussian
+struct SpinCosSumGaussian <: AbstractCosGaussian
 
     dims        :: Tuple{Int64, Int64, Int64}
-    params      :: SpinCosGaussianParams
+    params      :: CosGaussianParams
 
-    function SpinCosSumGaussian{D, V, S}( k :: Array{Vector{Float64}, 1}, 
-                                   α :: Vector{Float64}, 
-                                   σ :: Array{Vector{Float64}, 1}, 
-                                   μ :: Array{Vector{Float64}, 1},
-                                   δ :: Vector{Float64} = [1.0] ) where {D,V,S}
+    function SpinCosSumGaussian( k :: Array{Vector{Float64}, 1}, 
+                                 α :: Vector{Float64}, 
+                                 σ :: Array{Vector{Float64}, 1}, 
+                                 μ :: Array{Vector{Float64}, 1},
+                                 δ :: Vector{Float64} = [1.0] )
 
-        dims   = (D, V, S)
-        params = SpinCosGaussianParams( dims, k, α, σ, μ, δ )
+        dims   = (1, 1)
+        params = CosGaussianParams( dims, k, α, σ, μ, δ )
 
         new( dims, params )
     end
