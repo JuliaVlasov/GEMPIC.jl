66c27ab18c7dabc35459e14246e32fbdab4511d5
Documenter.jl <documenter@juliadocs.github.io>
2026-05-04 08:15:43 +0000

    build based on 47dc852

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -11,40 +11,51 @@ requirejs.config({
     'highlight-julia-repl': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/julia-repl.min',
   },
   shim: {
-  "highlight-julia": {
-    "deps": [
-      "highlight"
-    ]
-  },
-  "mathjax": {
-    "exports": "MathJax"
-  },
   "headroom-jquery": {
     "deps": [
       "jquery",
       "headroom"
     ]
   },
+  "highlight-julia": {
+    "deps": [
+      "highlight"
+    ]
+  },
   "highlight-julia-repl": {
     "deps": [
       "highlight"
     ]
+  },
+  "mathjax": {
+    "exports": "MathJax"
   }
 }});
 ////////////////////////////////////////////////////////////////////////////////
 require(['mathjax'], function(MathJax) {
 MathJax.Hub.Config({
+  "TeX": {
+    "Macros": {},
+    "equationNumbers": {
+      "autoNumber": "AMS"
+    }
+  },
+  "config": [
+    "MMLorHTML.js"
+  ],
+  "extensions": [
+    "MathMenu.js",
+    "MathZoom.js",
+    "TeX/AMSmath.js",
+    "TeX/AMSsymbols.js",
+    "TeX/autobold.js",
+    "TeX/autoload-all.js"
+  ],
   "jax": [
     "input/TeX",
     "output/HTML-CSS",
     "output/NativeMML"
   ],
-  "TeX": {
-    "equationNumbers": {
-      "autoNumber": "AMS"
-    },
-    "Macros": {}
-  },
   "tex2jax": {
     "inlineMath": [
       [
@@ -57,18 +68,7 @@ MathJax.Hub.Config({
       ]
     ],
     "processEscapes": true
-  },
-  "config": [
-    "MMLorHTML.js"
-  ],
-  "extensions": [
-    "MathMenu.js",
-    "MathZoom.js",
-    "TeX/AMSmath.js",
-    "TeX/AMSsymbols.js",
-    "TeX/autobold.js",
-    "TeX/autoload-all.js"
-  ]
+  }
 });
 
 })
